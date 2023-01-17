266689a30d40652210b1d2414ef18febcd2ed58e
Clara Situma <c.situma@factor-eleven.de>
2023-01-17 23:25:50 +0300

    Update: chage query according to BE change

--- src/test/Handlers/LoginHandler.test.ts
+++ src/test/Handlers/LoginHandler.test.ts
@@ -1,16 +1,22 @@
 import { LoginHandler } from "../../app/Handlers/LoginHandler"
-import { HTTP_CODES, HTTP_METHODS } from "../../app/Models/ServerModels";
+import { HTTP_CODES, HTTP_METHODS, SessionToken } from "../../app/Models/ServerModels";
 import { Utils } from "../../app/Utils/Utils";
 
-describe('Login handler test suite', () => { 
+describe('Login handler test suite', async () => { 
 
 let loginHandler:LoginHandler;
 
 const requestMock ={
     method:''
 }
-const responseMock ={ writeHead: jest.fn()}
-const authorizeMock ={}
+const responseMock = {
+    writeHead: jest.fn(),
+    write: jest.fn(),
+    statusCode: 0
+};
+const authorizerMock = {
+    generateToken: jest.fn()
+};
 
 const getRequestBodyMock = jest.fn()
 
@@ -18,21 +24,30 @@ beforeEach(()=>{
     loginHandler = new LoginHandler(
      requestMock as any,
     responseMock as any,
-    authorizeMock as any,
+    authorizerMock as any,
     )
+    requestMock.method= HTTP_METHODS.POST
     Utils.getRequestBody = getRequestBodyMock;
 })
 
 afterEach(()=>{
     jest.clearAllMocks();
 })
+
+const someSessionToken: SessionToken = {
+    tokenId: 'someTokenId',
+    userName: 'someUserName',
+    valid: true,
+    expirationTime: new Date(),
+    accessRights: [1, 2, 3]
+}
+
 //the options
 test('options request', async() => { 
     requestMock.method = HTTP_METHODS.OPTIONS
     await loginHandler.handleRequest();
     expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.OK)
  })
-
 
  //the break
  test('not handled http request', async() => { 
@@ -43,14 +58,43 @@ test('options request', async() => {
 
  //the POST Auth
 
- test.only('post request with valid body', async() => { 
- requestMock.method= HTTP_METHODS.POST
-    // await loginHandler.handleRequest();
-getRequestBodyMock.mockReturnValueOnce({
+ test('post request with valid body', async() => { 
+
+ getRequestBodyMock.mockReturnValueOnce({
     username: 'someUser',
     password:'password'
 })
-  })
+  authorizerMock.generateToken.mockReturnValueOnce(someSessionToken);
+  await loginHandler.handleRequest();
+  expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
+  expect(responseMock.writeHead).toBeCalledWith(
+      HTTP_CODES.CREATED, { 'Content-Type': 'application/json' }
+  );
+  expect(responseMock.write).toBeCalledWith(JSON.stringify(someSessionToken));
 
 
  })
+
+
+ test('post request with invalid body', async() => { 
+  
+    getRequestBodyMock.mockReturnValueOnce({
+       username: 'someUser',
+       password:'password'
+   })
+     authorizerMock.generateToken.mockReturnValueOnce(null);
+     await loginHandler.handleRequest();
+     expect(responseMock.statusCode).toBe(HTTP_CODES.NOT_fOUND);
+     expect(responseMock.write).toBeCalledWith('wrong username or password');
+    })
+
+    test('post request with unexpected error', async() => { 
+        
+        getRequestBodyMock.mockRejectedValueOnce(new Error('something went wrong!'))
+        authorizerMock.generateToken.mockReturnValueOnce(null);
+        await loginHandler.handleRequest();
+        expect(responseMock.statusCode).toBe(HTTP_CODES.INTERNAL_SERVER_ERROR);
+        expect(responseMock.write).toBeCalledWith('Internal error: something went wrong!');
+        })
+
+})
